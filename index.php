9bd182affb71e2e49be3caa54635bdbb33d34955
sam <sa>
2023-02-15 14:24:31 -0800

    css phone update

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php include_once("index.html"); ?>
+<?php 
+
+include_once("index.html");
+
+// hit this page
+// UPDATE Table SET Thing = Thing + 1;
+//  some query function to call this SQL
+
+
+?>
